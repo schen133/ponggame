509620cf41e26585dba6335243e437e79d435966
Mokin Lee <molee1354@gmail.com>
2024-02-10 09:58:51 -0500

    add: added separate functions that determine ball contact with player

--- physics.js
+++ physics.js
@@ -1,6 +1,29 @@
-export default function myPhysics(x1, x2, y1, y2) {
-    console.log("x1 is " + x1);
-    console.log("x2 is " + x2);
-    console.log("y1 is " + y1);
-    console.log("y2 is " + y2);
+/**
+ * @brief Function to see if the ball makes contact with player1.
+ *   -> `player1` is to the left of `player2`
+ *   -> check 'top', 'bottom', and 'right' of player1. 
+ * 
+ * @returns bool True if ball is in contact with player 1
+ */
+export function p1Contact(bx, by, br, px, py, pr) {
+  // check right, bottom, top in that order
+  if ( (bx-br) < px && by < (py+pr) && by > (py-pr) ) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * @brief Function to see if the ball makes contact with player2.
+ *   -> `player2` is to the right of `player1`
+ *   -> check 'top', 'bottom', and 'left' of player2. 
+ * 
+ * @returns bool True if ball is in contact with player 2
+ */
+export function p2Contact(bx, by, br, px, py, pr) {
+  // check left, bottom, top in that order
+  if ( (bx+br) > px && by < (py+pr) && by > (py-pr) ) {
+    return true;
+  }
+  return false;
 }
